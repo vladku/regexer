02a0015beabcc8d7b464976e8b5b84050d5ad469
vladku <vladku13@gmail.com>
2020-03-14 20:24:43 +0200

    Fix logic. Add docs

--- regexer.py
+++ regexer.py
@@ -1,58 +1,71 @@
 import re
 
 class RegexerString(str):
-  def __sub__(self, other):
-      if isinstance(other, str):
-          return re.sub(other, "", self) 
-      else:
-          raise Exception(f"Not supported!")  
+    """Simplify working with regex.
 
-  def __truediv__(self, other):
-      if isinstance(other, str):
-          return list(filter(lambda item: item, re.split(other, self)))
-      else:
-          raise Exception(f"Not supported!")
+    Examples:
+        ::
+            >>> RegexerString("Test01 string sentence02")[r"(?P<num>\d+)", 'num']
+            ['01', '02']
+            >>> RegexerString("Test01 string sentence02")[r"(?P<num>\d+)", 1, 'num']
+            '02'
+            >>> RegexerString("Test01 string sentence02") / r"\w\d{2}"
+            ['Tes', ' string sentenc']
+            >>> RegexerString("Test01 string sentence02") - r"\d"
+            'Test string sentence'
+  
+    Raises:
+        Exception: type not supported.
+        Exception: no item found by index.
+    """
+    def __sub__(self, other):
+        if isinstance(other, str):
+            return re.sub(other, "", self) 
+        else:
+            raise Exception(f"Not supported!")  
 
-  def __getitem__(self, index):
-      if isinstance(index, str):
-          return [x.groupdict() for x in re.finditer(index, self)]
-      r = [x.groupdict() for x in re.finditer(index[0], self)]
-      if len(index) == 3:
-          if len(r) <= index[1]:
-              raise Exception(f"There is no item on index {index[1]}")
-          return r[index[1]][index[2]]
-      elif len(index) == 2 and len(r) == 1:
-          return r[0][index[1]]
-      else:
-        return [x[index[1]] for x in r]
+    def __truediv__(self, other):
+        if isinstance(other, str):
+            return list(filter(lambda item: item, re.split(other, self)))
+        else:
+            raise Exception(f"Not supported!")
+
+    def __getitem__(self, index):
+        if isinstance(index, str):
+            return [x.groupdict() for x in re.finditer(index, self)]
+        r = [x.groupdict() for x in re.finditer(index[0], self)]
+        if len(index) == 3:
+            if len(r) <= index[1]:
+                raise Exception(f"There is no item on index {index[1]}")
+            return r[index[1]][index[2]]
+        elif len(index) == 2 and len(r) == 1:
+            return r[0][index[1]]
+        else:
+            return [x[index[1]] for x in r]
+
+class RegexerList(list):
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return list(self)[index]
+        if isinstance(index, str):
+            return [x[index] for x in self]
 
 class Regexer:
-  """Simplify working with regex.
+    """Simplify working with regex.
 
-  Examples:
-      ::
-          >>> Regexer("Test01 string sentence02")[r"(?P<num>\d+)", 'num']
-          ['01', '02']
-          >>> Regexer("Test01 string sentence02")[r"(?P<num>\d+)", 1, 'num']
-          '02'
-          >>> Regexer("Test01 string sentence02") / r"\w\d{2}"
-          ['Tes', ' string sentenc']
-          >>> Regexer("Test01 string sentence02") - r"\d"
-          'Test string sentence'
-  
-  Raises:
-      Exception: type not supported.
-      Exception: no item found by index.
-  """
-  def __init__(self, r):
-      self.r = r
+    Examples:
+        ::
+            >>> Regexer([r"(?P<num>\d+)")("Test01 string sentence02")['num']
+            ['01', '02']
+            >>> Regexer([r"(?P<num>\d+)")("Test01 string sentence02")[1]['num']
+            '02'
+    
+    Raises:
+        Exception: type not supported.
+        Exception: no item found by index.
+    """
+    def __init__(self, r):
+        self.r = r
 
-  def __call__(self, s):
-      return RegexerString(s)[self.r]
-
-r = Regexer(r"(?P<num>\d+)")
-print(r("Test01 string sentence02")[1]["num"])
-print(RegexerString("Test01 string sentence02")[r"(?P<num>\d+)", 'num'])
-print(RegexerString("Test01 string sentence02") / r"\w\d{2}")
-
-
+    def __call__(self, s):
+        return RegexerList(RegexerString(s)[self.r])
--- test_regexer.py
+++ test_regexer.py
@@ -1,15 +1,24 @@
-from regexer import RegexerString as r
+from regexer import Regexer as r
+from regexer import RegexerString as s
 
 def test_match():
-    assert r("Test01 string sentence02")[r"(?P<num>\d+)", 'num'] == ['01', '02']
+    assert s("Test01 string sentence02")[r"(?P<num>\d+)", 'num'] == ['01', '02']
 
 def test_match_index():
-    assert r("Test01 string sentence02")[r"(?P<num>\d+)", 1, 'num'] == '02'
+    assert s("Test01 string sentence02")[r"(?P<num>\d+)", 1, 'num'] == '02'
 
 def test_split():
-    result = r("Test01 string sentence02") / r"\w\d{2}"
+    result = s("Test01 string sentence02") / r"\w\d{2}"
     assert ["Tes", " string sentenc"] == result
 
 def test_remove():
-    result = r("Test01 string sentence02") - r"\d"
+    result = s("Test01 string sentence02") - r"\d"
     assert "Test string sentence" == result
+    
+def test_regexer():
+    num_regex = r(r"(?P<num>\d+)")
+    one = num_regex("Test01 string sentence02")
+    assert one == [{'num': '01'}, {'num': '02'}]
+    assert one["num"] == ['01', '02']
+    assert one[0] == {"num": '01'}
+    assert num_regex("Test01 string sentence03")["num"] == ['01', '03']
